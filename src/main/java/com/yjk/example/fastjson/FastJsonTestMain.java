6dcee33801bab7d91d6b0748cbf935e1292b65df
jkyu@haiyi-info.com <jkyu@haiyi-info.com>
2018-09-18 08:57:36 +0800

    [ADD] fastjson 支持byte数组

--- src/main/java/com/yjk/example/fastjson/FastJsonTestMain.java
+++ src/main/java/com/yjk/example/fastjson/FastJsonTestMain.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,14 +17,14 @@ import java.util.List;
  **/
 public class FastJsonTestMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
       Person person = new Person();
       person.setName("yjk");
       person.setSex("male");
 
         List<Addr> addrs = new ArrayList<Addr>();
         Addr tempAddr = new Addr();
-        tempAddr.setCity("china");
+        tempAddr.setCity("china中国");
         tempAddr.setStreet("xxxxx");
         addrs.add(tempAddr);
 
@@ -36,9 +37,11 @@ public class FastJsonTestMain {
 
         String jsonStr = JSON.toJSONString(person, SerializerFeature.WriteClassName);
         System.out.println(jsonStr);
+        byte[] jsonStrBytes= jsonStr.getBytes("UTF-8");
         ParserConfig parserConfig = ParserConfig.getGlobalInstance();
         parserConfig.setAutoTypeSupport(true);
-        Person b = (Person) JSON.parse(jsonStr,parserConfig);
+        String tempJsonStr = new String(jsonStrBytes,"UTF-8");
+        Person b = (Person) JSON.parse(tempJsonStr,parserConfig);
         System.out.println(b);
     }
 }
